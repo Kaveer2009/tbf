546c85110e7ad1314b59f96157cd014a0e68711e
Khatarnaak Kaveer <86919827+Kaveer2009@users.noreply.github.com>
2023-05-02 23:14:39 +0530

    hi

--- app.js
+++ app.js
@@ -26,3 +26,21 @@ function scrollHeader(){
     if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+
+
+// nav op start
+
+    /* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+
+
+// nav op end
