524f59948123b4ea6db892095f17e46da627933b
Khatarnaak Kaveer <86919827+Kaveer2009@users.noreply.github.com>
2023-05-30 15:06:02 +0530

    hi

--- app.js
+++ app.js
@@ -55,30 +55,87 @@ var head_name = "";
 function check_product() {
     window.location = "view_product.html";
 }
-
+//function for wooden table
 function check_order_wooden_table() {
     window.location = "view_product.html";
     head_name = "Wooden Table";
-    
+
+    img_1 = "'images/example/wteg1.jpeg'";
+    img_2 = "'images/example/wteg2.png'";
+    img_3 = "'images/example/wteg3.jpeg'";
+    img_4 = "'images/example/wteg4.jpeg'";
+    img_5 = "'images/example/wteg5.png'";
+
+    localStorage.setItem("img_1", img_1)
+    localStorage.setItem("img_2", img_2)
+    localStorage.setItem("img_3", img_3)
+    localStorage.setItem("img_4", img_4)
+    localStorage.setItem("img_5", img_5)
+
+
+
     localStorage.setItem("Product Name", head_name);
     
-    set_head_name = localStorage.getItem("Product Name");
+    // set_head_name = localStorage.getItem("Product Name");
     document.getElementById("title").innerHTML = set_head_name;
     console.log(set_head_name);    
 
 }
 
 function start(){
-    head_name = "Wooden Table";
     
-    localStorage.setItem("Product Name", head_name);
+    
+    // localStorage.setItem("Product Name", head_name);
     
     set_head_name = localStorage.getItem("Product Name");
-    document.getElementById("title").innerHTML = set_head_name;
+    document.getElementById("product_name").innerHTML = set_head_name;
     console.log(set_head_name);
+    show_img_1 = localStorage.getItem("img_1");
+    show_img_2 = localStorage.getItem("img_2");
+    show_img_3 = localStorage.getItem("img_3");
+    show_img_4 = localStorage.getItem("img_4");
+    show_img_5 = localStorage.getItem("img_5");
 }
 
 start();
+
+
+function back_to_home() {
+    window.location = "index.html";
+}
+
+
+// changing show image
+
+function change_image_1() {
+    document.getElementById("image-show").innerHTML = "<img src="+show_img_1+" alt='Loading'>"
+    console.log(img_1);
+}
+
+function change_image_2() {
+    document.getElementById("image-show").innerHTML = "<img src="+show_img_2+" alt='Loading'>"
+}
+function change_image_3() {
+    document.getElementById("image-show").innerHTML = "<img src="+show_img_3+" alt='Loading'>"
+}
+function change_image_4() {
+    document.getElementById("image-show").innerHTML = "<img src="+show_img_4+" alt='Loading'>"
+}
+function change_image_5() {
+    document.getElementById("image-show").innerHTML = "<img src="+show_img_5+" alt='Loading'>"
+}
+
+
+
+function add_to_cart() {
+    localStorage.setItem("cart_no", 3)
+    cart_item_no = Number(localStorage.getItem("cart_no"));
+    final_cart_item_no = cart_item_no + 1;
+    console.log(final_cart_item_no);
+}
+
+
+
 
 
 function back_to_home() {
